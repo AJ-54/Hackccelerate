c744cd89c919da0852e39546b158dfc6c7d9e9b0
VolDemorT-acer <kunalsolanke1000@gmail.com>
2020-11-07 08:05:54 +0530

    heroku chat

--- hack72/asgi.py
+++ hack72/asgi.py
@@ -1,16 +1,16 @@
 
 import os
 import django
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.routing
-
-
-
-from channels.routing import get_default_application
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hack72.settings')
+
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+from channels.routing import get_default_application
+
 
 
 
